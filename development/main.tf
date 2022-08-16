46ccde5ab3cc4438550f0bcf7b9410791c13d0e1
RulosDSCM <danielceron584@gmail.com>
2022-08-16 16:00:21 -0500

    [ADD] - Jenkins and vpc infrastructure

--- development/main.tf
+++ development/main.tf
@@ -8,3 +8,22 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
+
+terraform {
+  backend "s3" {
+    bucket  = "terraform-state-daniel-tutorial-jenkins"
+    key     = "tutorial-jenkins/development/network/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+}
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    name = "jenkins-instance-main_vpc"
+  }
+}
